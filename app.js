2fd70c709425939e29a10173a10c90ee6973ed6c
Raphael THIBIERGE <raphael@thibierge.dev>
2021-02-08 07:59:52 +0100

    add dotenv

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+// configure.env
+require('dotenv').config();
+
+// configure express
 const express = require('express');
 const app = express();
-const server_port = 3000;
 
+// define env vars
+const SERVER_PORT = process.env.SERVER_PORT;
+
+// define main route
 app.get('/', (req, res) => {
     res.send('Hello World !');
 })
 
 // start listening incoming requests
-app.listen(server_port, () => {
-    console.log(`App listening on port ${server_port}`);
+app.listen(SERVER_PORT, () => {
+    console.log(`App url : http://localhost:${SERVER_PORT}/`);
 })
