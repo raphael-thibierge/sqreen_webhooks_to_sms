e6be9d2275727dbef5cd00627ad13c7e366b1aea
Raphael THIBIERGE <raphael@thibierge.dev>
2021-02-08 08:50:13 +0100

    integrate Twilio API to send SMS

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ require('dotenv').config();
 // configure express
 const express = require('express');
 const app = express();
-
-// define env vars
 const SERVER_PORT = process.env.SERVER_PORT;
+
+// configure twilio
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const FROM_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+const TO_PHONE_NUMBER = process.env.DEST_PHONE_NUMBER;
+const twilio_client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+// function to send sms with twilio, return a promise
+function send_sms(message_body){
+     return twilio_client.messages
+            .create({
+                body: message_body,
+                from: FROM_PHONE_NUMBER,
+                to: TO_PHONE_NUMBER
+            });
+}
 
 // define main route
 app.get('/', (req, res) => {
-    res.send('Hello World !');
+    send_sms('Hello world').then(twilio_response => {
+        res.send(`SMS content : ${twilio_response.body}`);
+    });
 })
 
 // start listening incoming requests
