f96980977c4cae543ce69038843805b8c507d631
Raphael THIBIERGE <raphael@thibierge.dev>
2021-02-14 22:09:44 +0100

    Improve promise.resolve in sendSms function

--- src/sendSms.js
+++ src/sendSms.js
@@ -10,7 +10,7 @@ const sendSms = (message_body) => {
 
     // TODO : remove this very ugly way to prevent sending SMS during tests
     if (process.env.APP_ENV === 'test'){
-        return new Promise((resolve, reject)=>{resolve(message_body)});
+        return Promise.resolve(message_body);
     }
 
     const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
